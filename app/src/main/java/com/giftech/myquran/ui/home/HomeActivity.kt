25932df406770bbe918140951b118beb5bce3e9c
galihif <galihindra2401@mail.ugm.ac.id>
2021-09-16 10:06:15 +0700

    add HomeActivity.kt and add internet permission

--- app/src/main/java/com/giftech/myquran/ui/home/HomeActivity.kt
+++ app/src/main/java/com/giftech/myquran/ui/home/HomeActivity.kt
@@ -1,11 +1,12 @@
-package com.giftech.myquran
+package com.giftech.myquran.ui.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.giftech.myquran.R
 
-class SplashActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_home)
     }
 }
