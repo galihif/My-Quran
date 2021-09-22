4cb7dfd97739cf03c8e1d7550feb47532fc2096b
galihif <galihindra2401@mail.ugm.ac.id>
2021-09-22 09:51:53 +0700

    add onclicklistener when surah clicked

--- app/src/main/java/com/giftech/myquran/ui/home/SurahAdapter.kt
+++ app/src/main/java/com/giftech/myquran/ui/home/SurahAdapter.kt
@@ -1,10 +1,12 @@
 package com.giftech.myquran.ui.home
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.giftech.myquran.data.source.local.entity.SurahEntity
 import com.giftech.myquran.databinding.ItemSurahBinding
+import com.giftech.myquran.ui.surah.SurahActivity
 
 class SurahAdapter:RecyclerView.Adapter<SurahAdapter.SurahViewHolder>() {
 
@@ -35,6 +37,13 @@ class SurahAdapter:RecyclerView.Adapter<SurahAdapter.SurahViewHolder>() {
                 tvSurahName.text = surah.nama
                 tvSurahArab.text = surah.asma
                 tvSurahTypeAyat.text ="${surah.type} - ${surah.ayat} AYAT"
+
+
+                itemView.setOnClickListener {
+                    val intent = Intent(itemView.context, SurahActivity::class.java)
+                    intent.putExtra(SurahActivity.EXTRA_SURAH, surah.nomor)
+                    itemView.context.startActivity(intent)
+                }
             }
         }
     }
