4da9b05a18aba0662abb9ed93506419afa738a14
galihif <galihindra2401@mail.ugm.ac.id>
2021-09-25 21:27:34 +0700

    disable dark mode

--- app/src/main/java/com/giftech/myquran/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/giftech/myquran/ui/splash/SplashActivity.kt
@@ -3,6 +3,7 @@ package com.giftech.myquran.ui.splash
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.giftech.myquran.databinding.ActivitySplashBinding
 import com.giftech.myquran.ui.home.HomeActivity
 
@@ -14,6 +15,8 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         binding.btnGetStarted.setOnClickListener {
             val intent = Intent(this, HomeActivity::class.java)
