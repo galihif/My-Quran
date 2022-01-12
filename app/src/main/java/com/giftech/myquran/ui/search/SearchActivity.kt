afa3b623433c4a107e639966f14276dd3edc4ccd
galihif <galihindra2401@mail.ugm.ac.id>
2022-01-12 20:54:26 +0700

    add back button

--- app/src/main/java/com/giftech/myquran/ui/search/SearchActivity.kt
+++ app/src/main/java/com/giftech/myquran/ui/search/SearchActivity.kt
@@ -23,21 +23,22 @@ class SearchActivity : AppCompatActivity() {
 
         binding.etSearch.requestFocus()
 
-
-
         binding.etSearch.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
                 viewmodel.getSurahByName(s.toString()).observe(this@SearchActivity, {res ->
                     adapter.setList(res)
                 })
             }
-
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
             }
         })
+
+        binding.btnBack.setOnClickListener {
+            onBackPressed()
+        }
 
         with(binding.rvSurah){
             this.layoutManager = LinearLayoutManager(context)
