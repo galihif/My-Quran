4658ed959b4c891f269aa65db46ec1434269e306
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-12-06 15:08:07 +0700

    create list of surah

--- app/src/main/java/com/giftech/myquran/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/giftech/myquran/ui/screen/home/HomeScreen.kt
@@ -1,14 +1,17 @@
 package com.giftech.myquran.ui.screen.home
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -16,36 +19,85 @@ import androidx.compose.ui.unit.sp
 import com.giftech.myquran.R
 import com.giftech.myquran.ui.components.BoxLastRead
 import com.giftech.myquran.ui.components.TextTitle
+import com.giftech.myquran.ui.theme.Gray500
 import com.giftech.myquran.ui.theme.MyQuranTheme
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun HomeScreen() {
-    Surface(modifier = Modifier.fillMaxSize()) {
-        Column(
-            Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
+    Scaffold(
+        Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+    ) {
+        val itemList = (0..10).map { it.toString() }
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(4.dp)
         ) {
-            Row(
-                Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                TextTitle(text = R.string.title)
-                IconButton(onClick = {}) {
-                    Icon(imageVector = Icons.Default.Search, contentDescription = "Search")
+            item{
+                Column {
+                    Row(
+                        Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        TextTitle(text = R.string.title)
+                        IconButton(onClick = {}) {
+                            Icon(imageVector = Icons.Default.Search, contentDescription = "Search")
+                        }
+                    }
+                    Text(
+                        text = stringResource(id = R.string.salam),
+                        Modifier.padding(vertical = 16.dp),
+                        fontSize = 18.sp
+                    )
+                    BoxLastRead(
+                        surah = "Al-Baqarah",
+                        ayat = 8,
+                        onClick = {}
+                    )
+                    TextTitle(
+                        text = R.string.surah,
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                            .padding(vertical = 8.dp)
+                    )
                 }
             }
-            Text(
-                text = stringResource(id = R.string.salam),
-                Modifier.padding(vertical = 16.dp),
-                fontSize = 18.sp
+            items(itemList) {
+                SurahItem(it)
+            }
+        }
+    }
+}
+
+@Composable
+fun SurahItem(name: String) {
+    Row(
+        Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(Modifier.wrapContentSize()) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_bg_surah_number),
+                contentDescription = ""
             )
-            BoxLastRead(
-                surah = "Al-Baqarah",
-                ayat = 8,
-                onClick = {}
+            Text(name, Modifier.align(Alignment.Center))
+        }
+        Spacer(Modifier.width(8.dp))
+        Column(Modifier.weight(1f)) {
+            Text(
+                text = "Surah Name",
+                color = MaterialTheme.colors.primary
+            )
+            Text(
+                text = "Mekah {2} ayat",
+                color = Gray500
             )
         }
+        Text(
+            text = "Arabic Name",
+            color = MaterialTheme.colors.primary
+        )
     }
 }
 
