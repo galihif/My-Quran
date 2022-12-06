4c4b03a3e979316a88ab2b64ecad8c14b730829b
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-12-06 13:03:43 +0700

    add peach color

--- app/src/main/java/com/giftech/myquran/ui/theme/Color.kt
+++ app/src/main/java/com/giftech/myquran/ui/theme/Color.kt
@@ -3,6 +3,7 @@ package com.giftech.myquran.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
+val Purple500 = Color(0xFF672CBC)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+val Peach200 = Color(0xFFF9B091)
