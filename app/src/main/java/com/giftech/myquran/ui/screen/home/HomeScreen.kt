693f9693962ae0d2164d33fa7a6a0e9edb7a0ba5
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-12-06 15:56:35 +0700

    add arabic font

--- app/src/main/java/com/giftech/myquran/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/giftech/myquran/ui/screen/home/HomeScreen.kt
@@ -13,6 +13,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -94,8 +96,10 @@ fun SurahItem(name: String) {
             )
         }
         Text(
-            text = "Arabic Name",
-            color = MaterialTheme.colors.primary
+            text = " البقرة",
+            color = MaterialTheme.colors.primary,
+            fontFamily = FontFamily(Font(R.font.amiri_bold)),
+            fontSize = 22.sp
         )
     }
 }
