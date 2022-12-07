9d1964f355b10bc5422b91118cccf19ddcf52f57
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-12-07 21:00:35 +0700

    getdetail surah from remote

--- app/src/main/java/com/giftech/myquran/data/MainRepository.kt
+++ app/src/main/java/com/giftech/myquran/data/MainRepository.kt
@@ -1,11 +1,8 @@
 package com.giftech.myquran.data
 
 import com.giftech.myquran.data.model.Surah
-import com.giftech.myquran.data.source.local.entity.SurahEntity
 import com.giftech.myquran.data.source.remote.RemoteDataSource
-import com.giftech.myquran.data.source.remote.api.ApiService
 import com.giftech.myquran.data.source.remote.dto.toModel
-import com.giftech.myquran.data.source.remote.response.toEntity
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
@@ -18,5 +15,12 @@ class MainRepository @Inject constructor(
     suspend fun getListSurah(): Flow<List<Surah>> =
         flowOf(
             remote.getListSurah().map { it.toModel() }
+        )
+
+    suspend fun getDetailSurah(nomorSurah:Int):Flow<Surah> =
+        flowOf(
+            remote
+                .getDetailSurah(nomorSurah)
+                .toModel()
         )
 }
