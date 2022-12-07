2fd91f051f19a481cfab3beb92025446cbde94c0
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-12-07 16:15:14 +0700

    add purple900 and purple 100

--- app/src/main/java/com/giftech/myquran/ui/theme/Color.kt
+++ app/src/main/java/com/giftech/myquran/ui/theme/Color.kt
@@ -2,9 +2,11 @@ package com.giftech.myquran.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val Purple100 = Color(0xFFF9F5FD)
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF672CBC)
 val Purple700 = Color(0xFF3700B3)
+val Purple900 = Color(0xFF240F4F)
 val Teal200 = Color(0xFF03DAC5)
 val Peach200 = Color(0xFFF9B091)
 val Gray500 = Color(0xFF8789A3)
