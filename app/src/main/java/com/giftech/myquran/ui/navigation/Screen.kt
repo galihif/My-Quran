6195ae0e01c35b44ea5587b347c203df0c564f11
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-12-07 21:01:56 +0700

    renaming surah id to nomor surah

--- app/src/main/java/com/giftech/myquran/ui/navigation/Screen.kt
+++ app/src/main/java/com/giftech/myquran/ui/navigation/Screen.kt
@@ -4,8 +4,8 @@ package com.giftech.myquran.ui.navigation
 sealed class Screen(val route: String) {
     object Welcome : Screen("welcome")
     object Home : Screen("home")
-    object Surah : Screen("surah/{surahId}") {
-        fun createRoute(surahId: Int) = "surah/$surahId"
+    object Surah : Screen("surah/{nomorSurah}") {
+        fun createRoute(nomorSurah: Int) = "surah/$nomorSurah"
     }
     object Search : Screen("search")
 }
