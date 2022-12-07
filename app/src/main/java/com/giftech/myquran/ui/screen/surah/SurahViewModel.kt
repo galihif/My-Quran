39f7a197ef567119e5a55c18bd7026d802201a0f
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-12-07 21:03:02 +0700

    getdetailsurah from repository

---
+++ app/src/main/java/com/giftech/myquran/ui/screen/surah/SurahViewModel.kt
@@ -0,0 +1,51 @@
+package com.giftech.myquran.ui.screen.surah
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.giftech.myquran.data.MainRepository
+import com.giftech.myquran.data.Resource
+import com.giftech.myquran.data.model.Surah
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SurahViewModel
+@Inject constructor(
+    private val repository: MainRepository
+) : ViewModel() {
+
+    private var _nomorSurah = MutableStateFlow(1)
+
+    fun setNomorSurah(nomor:Int){
+        _nomorSurah.value = nomor
+        getDetailSurah()
+    }
+
+    private val _surah = MutableStateFlow<Resource<Surah>>(Resource.Loading())
+    val surah:StateFlow<Resource<Surah>> = _surah
+
+    private fun getDetailSurah(){
+        viewModelScope.launch {
+            repository.getDetailSurah(_nomorSurah.value)
+                .catch {
+                    _surah.value = Resource.Error(it.message.toString())
+                }.collect{
+                    _surah.value = Resource.Success(it)
+                }
+        }
+    }
+
+    private val _namaSurah = MutableStateFlow("")
+    val namaSurah:StateFlow<String> = _namaSurah
+
+    fun setNamaSurah(nama:String){
+        _namaSurah.value = nama
+    }
+
+
+
+}
