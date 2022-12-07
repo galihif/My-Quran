9a7fd0a82cb6a9bc8938271fa72a1e40dd33e461
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-12-07 21:02:36 +0700

    show detail surah and list ayat from viewmodel

--- app/src/main/java/com/giftech/myquran/ui/screen/surah/SurahScreen.kt
+++ app/src/main/java/com/giftech/myquran/ui/screen/surah/SurahScreen.kt
@@ -6,12 +6,17 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.giftech.myquran.data.Resource
+import com.giftech.myquran.data.model.Ayat
+import com.giftech.myquran.data.model.Surah
 import com.giftech.myquran.ui.components.BoxSurahHeader
 import com.giftech.myquran.ui.components.CardAyatHeader
 import com.giftech.myquran.ui.components.TitleBar
@@ -19,54 +24,79 @@ import com.giftech.myquran.ui.theme.*
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun SurahScreen() {
+fun SurahScreen(
+    viewModel: SurahViewModel = hiltViewModel(),
+    nomorSurah: Int = 1,
+    onBack: () -> Unit
+) {
     Scaffold(
         topBar = {
             TitleBar(
-                title = "{Nama Surah}",
-                onBack = {}
+                title = viewModel.namaSurah.collectAsState().value,
+                onBack = onBack
             )
         }
     ) {
-        SurahContent()
+        viewModel.setNomorSurah(nomorSurah)
+        viewModel.surah.collectAsState().value.let {
+            when (it) {
+                is Resource.Error -> {}
+                is Resource.Loading -> {}
+                is Resource.Success -> {
+                    val data = it.data
+                    if (data != null) {
+                        SurahContent(data)
+                        viewModel.setNamaSurah(data.nama)
+                    }
+                }
+            }
+        }
     }
 }
 
 @Composable
-fun SurahContent() {
+fun SurahContent(
+    surah: Surah
+) {
     LazyColumn(
         contentPadding = PaddingValues(16.dp),
         verticalArrangement = Arrangement.spacedBy(4.dp)
     ) {
         item {
-            BoxSurahHeader()
+            BoxSurahHeader(surah)
         }
-        item{
+        item {
             Spacer(Modifier.height(16.dp))
         }
-        items((0..10).map { it.toString() }){
+        items(surah.listAyat!!) {
             AyatItem(it)
         }
     }
 }
 
 @Composable
-fun AyatItem(ayat: String) {
+fun AyatItem(ayat: Ayat) {
     Column(Modifier.fillMaxWidth()) {
-        CardAyatHeader(ayat.toInt())
-        Text(
-            "بِسْمِ اللّٰهِ الرَّحْمٰنِ الرَّحِيْمِ",
-            Modifier.align(Alignment.End),
-            textAlign = TextAlign.End,
-            color = Purple900,
-            fontFamily = fontsArab,
-            fontSize = 18.sp
-        )
-        Text(
-            "{Surah Arti}",
-            fontSize = 16.sp,
-            color = Purple900,
-        )
+        CardAyatHeader(ayat.nomor)
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Text(
+                ayat.arab,
+                Modifier.align(Alignment.End),
+                textAlign = TextAlign.End,
+                color = Purple900,
+                fontFamily = fontsArab,
+                fontSize = 18.sp
+            )
+            Text(
+                ayat.arti,
+                fontSize = 16.sp,
+                color = Purple900,
+            )
+        }
     }
 }
 
@@ -74,6 +104,6 @@ fun AyatItem(ayat: String) {
 @Composable
 fun SurahScreenPreview() {
     MyQuranTheme {
-        SurahScreen()
+        SurahScreen() {}
     }
 }
