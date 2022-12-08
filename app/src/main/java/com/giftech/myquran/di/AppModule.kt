66a385e241ffa1f9b51851c88fc21d11be90ab7f
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-12-08 14:34:14 +0700

    add local data source injection

--- app/src/main/java/com/giftech/myquran/di/AppModule.kt
+++ app/src/main/java/com/giftech/myquran/di/AppModule.kt
@@ -1,11 +1,18 @@
 package com.giftech.myquran.di
 
+import android.content.Context
+import androidx.room.Room
 import com.giftech.myquran.data.MainRepository
+import com.giftech.myquran.data.source.local.LocalDataSource
+import com.giftech.myquran.data.source.local.preferences.Preferences
+import com.giftech.myquran.data.source.local.room.SurahDao
+import com.giftech.myquran.data.source.local.room.SurahDatabase
 import com.giftech.myquran.data.source.remote.RemoteDataSource
 import com.giftech.myquran.data.source.remote.api.ApiService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -16,6 +23,34 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun providePreferences(@ApplicationContext context: Context):Preferences =
+        Preferences(context.applicationContext)
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context):SurahDatabase  =
+        Room.databaseBuilder(
+            context.applicationContext,
+            SurahDatabase::class.java,
+            "Surah.db"
+        )
+            .fallbackToDestructiveMigration()
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideDao(database: SurahDatabase):SurahDao =
+        database.surahDao()
+
+    @Provides
+    @Singleton
+    fun provideLocal(dao:SurahDao, pref:Preferences):LocalDataSource  =
+        LocalDataSource(dao,pref)
+
+
     @Provides
     @Singleton
     fun provideApiService():ApiService{
@@ -40,5 +75,8 @@ object AppModule {
 
     @Provides
     @Singleton
-    fun provideRepository(remote:RemoteDataSource) = MainRepository(remote)
+    fun provideRepository(
+        local:LocalDataSource,
+        remote:RemoteDataSource
+    ) = MainRepository(local,remote)
 }
