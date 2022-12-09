79e7f7d195dc7aa47fc64ba5372f7781cd94e3e8
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-12-09 20:04:07 +0700

    propagate experimental api

--- app/src/main/java/com/giftech/myquran/ui/screen/search/SearchScreen.kt
+++ app/src/main/java/com/giftech/myquran/ui/screen/search/SearchScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -19,6 +20,7 @@ import com.giftech.myquran.ui.components.SearchBar
 import com.giftech.myquran.ui.components.SurahItem
 import com.giftech.myquran.ui.theme.MyQuranTheme
 
+@ExperimentalComposeUiApi
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun SearchScreen(
@@ -71,6 +73,7 @@ fun SearchResult(
     }
 }
 
+@ExperimentalComposeUiApi
 @Preview
 @Composable
 fun SearchPreview() {
