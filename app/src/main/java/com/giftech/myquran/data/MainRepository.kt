724d29fdb3a7d0cfd44e26373ade43795e57095c
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-12-09 16:50:34 +0700

    add room caching getListSurah

--- app/src/main/java/com/giftech/myquran/data/MainRepository.kt
+++ app/src/main/java/com/giftech/myquran/data/MainRepository.kt
@@ -6,34 +6,52 @@ import com.giftech.myquran.data.model.toEntity
 import com.giftech.myquran.data.source.local.LocalDataSource
 import com.giftech.myquran.data.source.local.entity.toModel
 import com.giftech.myquran.data.source.remote.RemoteDataSource
+import com.giftech.myquran.data.source.remote.dto.toEntity
 import com.giftech.myquran.data.source.remote.dto.toModel
+import com.giftech.myquran.utils.networkBoundResource
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
 class MainRepository @Inject constructor(
-    private val local:LocalDataSource,
+    private val local: LocalDataSource,
     private val remote: RemoteDataSource
 ) {
 
-    suspend fun getListSurah(): Flow<List<Surah>> =
-        flowOf(
-            remote.getListSurah().map { it.toModel() }
+    fun getListSurah(): Flow<Resource<List<Surah>>> =
+        networkBoundResource(
+            query = {
+                flow {
+                    local.getListSurah().collect {
+                        emit(it.map { surahEntity -> surahEntity.toModel() })
+                    }
+                }
+            },
+            shouldFetch = {
+                it.isEmpty()
+            },
+            fetch = {
+                remote.getListSurah()
+            },
+            saveFetchResult = { listSurahDto ->
+                local.insertNewSurah(listSurahDto.map { surahDto -> surahDto.toEntity() })
+            }
         )
 
-    suspend fun getDetailSurah(nomorSurah:Int):Flow<Surah> =
+    suspend fun getDetailSurah(nomorSurah: Int): Flow<Surah> =
         flowOf(
             remote
                 .getDetailSurah(nomorSurah)
                 .toModel()
         )
 
-    suspend fun getLastRead():Flow<LastRead> =
+    suspend fun getLastRead(): Flow<LastRead> =
         flowOf(
             local.getLastAyat().toModel()
         )
 
-    fun setLastRead(lastRead: LastRead){
+    fun setLastRead(lastRead: LastRead) {
         local.setLastAyat(lastRead.toEntity())
     }
 }
