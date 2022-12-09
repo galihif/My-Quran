d7400dea0fc688888df8dc0eb84e0d90f2e97618
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-12-09 21:44:47 +0700

    delete unused

--- app/src/main/java/com/giftech/myquran/data/source/local/entity/LastReadAyatEntity.kt
+++ app/src/main/java/com/giftech/myquran/data/source/local/entity/LastReadAyatEntity.kt
@@ -1,17 +1,14 @@
 package com.giftech.myquran.data.source.local.entity
 
-import android.os.Parcelable
 import com.giftech.myquran.data.model.LastRead
-import kotlinx.parcelize.Parcelize
 
-@Parcelize
-class LastReadAyatEntity (
-    var nomorAyat:Int = 0,
-    var nomorSurah:Int = 0,
-    var namaSurah:String =""
-        ):Parcelable
+class LastReadAyatEntity(
+    var nomorAyat: Int = 0,
+    var nomorSurah: Int = 0,
+    var namaSurah: String = ""
+)
 
-fun LastReadAyatEntity.toModel():LastRead =
+fun LastReadAyatEntity.toModel(): LastRead =
     LastRead(
         nomorAyat = nomorAyat,
         nomorSurah = nomorSurah,
