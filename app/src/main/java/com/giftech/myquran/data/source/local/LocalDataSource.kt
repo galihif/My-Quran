cb835725f395d884ccdddead5cbdca7b00380cca
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-12-09 16:49:32 +0700

    add insertNewSurah func

--- app/src/main/java/com/giftech/myquran/data/source/local/LocalDataSource.kt
+++ app/src/main/java/com/giftech/myquran/data/source/local/LocalDataSource.kt
@@ -1,28 +1,38 @@
 package com.giftech.myquran.data.source.local
 
 import androidx.lifecycle.LiveData
+import androidx.room.withTransaction
 import com.giftech.myquran.data.source.local.entity.LastReadAyatEntity
 import com.giftech.myquran.data.source.local.entity.SurahEntity
 import com.giftech.myquran.data.source.local.preferences.Preferences
 import com.giftech.myquran.data.source.local.room.SurahDao
+import com.giftech.myquran.data.source.local.room.SurahDatabase
 import javax.inject.Inject
 
 class LocalDataSource @Inject constructor(
     private val surahDao: SurahDao,
-    private val preferences: Preferences
+    private val preferences: Preferences,
+    private val db:SurahDatabase
 ){
     companion object {
         private var instance: LocalDataSource? = null
 
-        fun getInstance(surahDao: SurahDao, preferences: Preferences): LocalDataSource =
+        fun getInstance(surahDao: SurahDao, preferences: Preferences, db:SurahDatabase): LocalDataSource =
             instance ?: synchronized(this) {
-                instance ?: LocalDataSource(surahDao,preferences)
+                instance ?: LocalDataSource(surahDao,preferences,db)
             }
     }
 
     fun getAllSurah():LiveData<List<SurahEntity>> = surahDao.getAllSurah()
 
-    fun insertSurah(listSurah: List<SurahEntity>) = surahDao.insertSurah(listSurah)
+    fun getListSurah() = surahDao.getListSurah()
+
+    suspend fun insertNewSurah(listSurah: List<SurahEntity>){
+        db.withTransaction {
+            surahDao.deleteAllSurah()
+            surahDao.insertSurah(listSurah)
+        }
+    }
 
     fun getSurahByName(keyword:String):LiveData<List<SurahEntity>> = surahDao.getSurahByName(keyword)
 
