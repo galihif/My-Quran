4ea35a4130248f360cd769ca7de68915320947b6
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-12-09 20:03:55 +0700

    auto show keyboard in launcheffect

--- app/src/main/java/com/giftech/myquran/ui/components/TopBarComp.kt
+++ app/src/main/java/com/giftech/myquran/ui/components/TopBarComp.kt
@@ -8,10 +8,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -38,14 +40,17 @@ fun TitleBar(
     )
 }
 
+@ExperimentalComposeUiApi
 @Composable
 fun SearchBar(
     keyword: String = "",
     onBack: () -> Unit,
     onValueChange: (String) -> Unit
 ) {
+    val keyboardController = LocalSoftwareKeyboardController.current
     val focusRequester = remember { FocusRequester() }
     LaunchedEffect(Unit) {
+        keyboardController?.show()
         focusRequester.requestFocus()
     }
     Row(
