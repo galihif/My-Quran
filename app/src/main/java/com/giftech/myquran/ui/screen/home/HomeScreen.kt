a94d2a1068faf3d11cc7dd9b22d33ec2d4a2fe87
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-12-09 19:23:34 +0700

    add onsearch click

--- app/src/main/java/com/giftech/myquran/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/giftech/myquran/ui/screen/home/HomeScreen.kt
@@ -2,8 +2,6 @@ package com.giftech.myquran.ui.screen.home
 
 import android.annotation.SuppressLint
 import android.util.Log
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -16,7 +14,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,17 +24,17 @@ import com.giftech.myquran.data.Resource
 import com.giftech.myquran.data.model.LastRead
 import com.giftech.myquran.data.model.Surah
 import com.giftech.myquran.ui.components.BoxLastRead
+import com.giftech.myquran.ui.components.SurahItem
 import com.giftech.myquran.ui.components.TextTitle
-import com.giftech.myquran.ui.theme.Gray500
 import com.giftech.myquran.ui.theme.MyQuranTheme
-import com.giftech.myquran.ui.theme.fontsArab
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun HomeScreen(
     viewModel: HomeViewModel = hiltViewModel(),
     onSurahClick: (Int) -> Unit = {},
-    onLastReadClick: (Int) -> Unit = {}
+    onLastReadClick: (Int) -> Unit = {},
+    onSearchClick: () -> Unit = {},
 ) {
     val listSurah = remember {
         viewModel.listSurah
@@ -67,7 +64,8 @@ fun HomeScreen(
                             onSurahClicked = onSurahClick,
                             onLastReadClick = {
                                 onLastReadClick(lastRead.value.nomorSurah)
-                            }
+                            },
+                            onSearchClick = onSearchClick
                         )
                     }
                 }
@@ -81,7 +79,8 @@ fun HomeContent(
     listSurah: List<Surah>,
     lastRead: LastRead,
     onSurahClicked: (Int) -> Unit,
-    onLastReadClick: () -> Unit
+    onLastReadClick: () -> Unit,
+    onSearchClick: () -> Unit,
 ) {
     LazyColumn(
         Modifier
@@ -96,7 +95,7 @@ fun HomeContent(
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     TextTitle(text = R.string.title)
-                    IconButton(onClick = {}) {
+                    IconButton(onClick = onSearchClick) {
                         Icon(imageVector = Icons.Default.Search, contentDescription = "Search")
                     }
                 }
@@ -127,43 +126,6 @@ fun HomeContent(
     }
 }
 
-@Composable
-fun SurahItem(
-    surah: Surah,
-    onSurahClicked: (Int) -> Unit
-) {
-    Row(
-        Modifier
-            .fillMaxWidth()
-            .clickable { onSurahClicked(surah.nomor) },
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Box(Modifier.wrapContentSize()) {
-            Image(
-                painter = painterResource(id = R.drawable.ic_bg_surah_number),
-                contentDescription = ""
-            )
-            Text(surah.nomor.toString(), Modifier.align(Alignment.Center))
-        }
-        Spacer(Modifier.width(8.dp))
-        Column(Modifier.weight(1f)) {
-            Text(
-                text = surah.nama,
-                color = MaterialTheme.colors.primary
-            )
-            Text(
-                text = "Mekah ${surah.jumlahAyat} ayat",
-                color = Gray500
-            )
-        }
-        Text(
-            text = surah.asma,
-            color = MaterialTheme.colors.primary,
-            fontFamily = fontsArab,
-            fontSize = 22.sp
-        )
-    }
-}
 
 @Preview
 @Composable
