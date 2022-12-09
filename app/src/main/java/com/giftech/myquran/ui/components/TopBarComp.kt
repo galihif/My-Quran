350733b99195043ff414e2935264c935467656e0
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-12-09 19:27:43 +0700

    create SearchBar Comp

--- app/src/main/java/com/giftech/myquran/ui/components/TopBarComp.kt
+++ app/src/main/java/com/giftech/myquran/ui/components/TopBarComp.kt
@@ -1,13 +1,17 @@
 package com.giftech.myquran.ui.components
 
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.giftech.myquran.R
 
 @Composable
 fun TitleBar(
@@ -29,3 +33,35 @@ fun TitleBar(
         backgroundColor = Color.White
     )
 }
+
+@Composable
+fun SearchBar(
+    keyword: String = "",
+    onBack: () -> Unit,
+    onValueChange: (String) -> Unit
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start
+    ) {
+        IconButton(onClick = onBack) {
+            Icon(
+                imageVector = Icons.Filled.ArrowBack,
+                contentDescription = ""
+            )
+        }
+        TextField(
+            modifier = Modifier.fillMaxWidth().padding(end = 16.dp),
+            value = keyword,
+            onValueChange = onValueChange,
+            singleLine = true,
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = MaterialTheme.colors.surface
+            ),
+            placeholder = {
+                Text(stringResource(R.string.search_surah))
+            }
+        )
+    }
+}
+
